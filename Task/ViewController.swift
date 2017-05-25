fe45052f3ef7f9f77d2be03aa66073c5aec933ff
yyfan <yyfan1994@gmail.com>
2017-05-24 18:43:31 -0700

    create repo

---
+++ Task/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by iGuest on 5/24/17.
+//  Copyright © 2017 yyfan. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ViewController: UIViewController {
+    
+    let rootRef = Database.database().reference()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
